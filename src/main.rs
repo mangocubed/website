7b5f13742ea494a5fbf9125a049798ff00b6f8e3
Javier E. <jaedns@gmail.com>
2025-09-19 14:44:33 -0300

    Use hash segment for pages

--- src/main.rs
+++ src/main.rs
@@ -3,62 +3,40 @@ use dioxus::prelude::*;
 mod components;
 mod pages;
 
-use pages::{HomePage, PrivacyPage, TermsPage};
+use pages::{Page, PageSlug};
 
 #[derive(Debug, Clone, Routable, PartialEq)]
 #[rustfmt::skip]
 #[allow(clippy::enum_variant_names)]
 enum Routes {
     #[layout(Layout)]
-        #[route("/")]
-        HomePage {},
-        #[route("/privacy")]
-        PrivacyPage {},
-        #[route("/terms")]
-        TermsPage {},
+        #[route("/#:slug")]
+        Page { slug: PageSlug },
 }
 
 impl Routes {
     fn home() -> Self {
-        Self::HomePage {}
+        Self::Page { slug: PageSlug::Home }
     }
 
     fn privacy() -> Self {
-        Self::PrivacyPage {}
+        Self::Page {
+            slug: PageSlug::Privacy,
+        }
     }
 
     fn terms() -> Self {
-        Self::TermsPage {}
+        Self::Page { slug: PageSlug::Terms }
     }
 }
 
 const FAVICON_ICO: Asset = asset!("assets/favicon.ico");
+const ICON_SVG: Asset = asset!("assets/icon.svg");
 const LOGO_SVG: Asset = asset!("assets/logo.svg");
 const STYLE_CSS: Asset = asset!("assets/style.css");
 
 fn main() {
-    dioxus::LaunchBuilder::new()
-        // Set the server config only if we are building the server target
-        .with_cfg(server_only! {
-            ServeConfig::builder()
-                // Enable incremental rendering
-                .incremental(
-                    dioxus::server::IncrementalRendererConfig::new()
-                        // Store static files in the public directory where other static assets like wasm are stored
-                        .static_dir(
-                            std::env::current_exe()
-                                .unwrap()
-                                .parent()
-                                .unwrap()
-                                .join("public")
-                        )
-                        // Don't clear the public folder on every build. The public folder has other files including the wasm
-                        // binary and static assets required for the app to run
-                        .clear_cache(false)
-                )
-                .enable_out_of_order_streaming()
-        })
-        .launch(App);
+    dioxus::launch(App);
 }
 
 #[component]
@@ -66,6 +44,7 @@ fn App() -> Element {
     rsx! {
         document::Link { rel: "icon", href: FAVICON_ICO }
         document::Link { rel: "stylesheet", href: STYLE_CSS }
+
         Router::<Routes> {}
     }
 }
@@ -76,7 +55,8 @@ fn Layout() -> Element {
         div { class: "navbar bg-base-300",
             div { class: "navbar-start",
                 Link { class: "p-2 font-bold", to: Routes::home(),
-                    img { class: "h-8", src: LOGO_SVG }
+                    img { class: "h-[36px] sm:hidden", src: ICON_SVG }
+                    img { class: "h-[36px] max-sm:hidden", src: LOGO_SVG }
                 }
             }
 
