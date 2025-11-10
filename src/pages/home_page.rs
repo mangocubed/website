538a2339dd6771a63f295f30a704c600796786dc
Javier E. <jaedns@gmail.com>
2025-11-10 15:56:20 -0300

    Use bigger fonts for titles (#10)

--- src/pages/home_page.rs
+++ src/pages/home_page.rs
@@ -1,6 +1,6 @@
 use dioxus::prelude::*;
 
-use sdk::app::components::{H2, Mango3Logo, PageTitle};
+use sdk::app::components::{Mango3Logo, PageTitle};
 
 #[component]
 pub fn FakeHomePage() -> Element {
@@ -16,29 +16,31 @@ pub fn HomePage() -> Element {
             div { class: "hero bg-base-200 p-4 rounded-2xl",
                 div { class: "hero-content text-center",
                     div {
-                        h1 { class: "h1 text-3xl", "Hi there!" }
+                        h1 { class: "h1 text-4xl", "Hi there!" }
 
                         div { class: "flex flex-wrap gap-2 mb-2 items-center justify-center",
-                            span { class: "text-xl font-bold", "Welcome to " }
+                            span { class: "text-2xl font-bold", "Welcome to " }
 
-                            Mango3Logo { class: "h-5 w-auto" }
+                            Mango3Logo { class: "h-6 w-auto" }
                         }
 
-                        p { "We are a small group of people building some cool stuff." }
+                        p { class: "text-xl",
+                            "We are a small group of people building some cool stuff."
+                        }
                     }
                 }
             }
         }
 
         section { class: "mt-8",
-            H2 { "Our Products" }
+            h2 { class: "h2 text-3xl", "Our Products" }
 
             div { class: "card card-border",
                 div { class: "card-body flex-row items-center justify-between",
                     div {
-                        h2 { class: "card-title h2", "Drive" }
+                        h3 { class: "h3 card-title text-2xl", "Drive" }
 
-                        p { "A cloud storage solution" }
+                        p { class: "text-xl", "A cloud storage solution" }
                     }
 
                     a {
